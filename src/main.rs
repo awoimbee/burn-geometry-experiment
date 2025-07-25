d56887cc3fc00469a9aaeb30b004cde75d0b610e
Arthur Woimbée <arthur.woimbee@gmail.com>
2025-07-25 09:26:11 +0200

    broken addition of clap

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,32 @@ mod inference;
 mod model;
 mod training;
 
-use std::env;
 
 use burn::backend::{Autodiff, Wgpu};
 use burn::optim::AdamConfig;
+use clap::{Parser, Subcommand};
 
 use crate::model::GeometryAutoEncoderConfig;
 use crate::training::TrainingConfig;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Train the MNIST model.
+    Train {},
+    /// Generate a geometry from latent parameters
+    Generate {
+        /// Path to the directory where the model artifacts are saved.
+        #[arg(short, long)]
+        parameters: Vec<f32>,
+    },
+}
 
 /// Main function to run the training and inference.
 ///
@@ -19,24 +38,23 @@ fn main() {
     type MyBackend = Wgpu<f32, i32>;
     type MyAotudiffBackend = Autodiff<MyBackend>;
 
+    let cli = Cli::parse();
     let device = burn::backend::wgpu::WgpuDevice::default();
-
     let artifact_dir = "artifacts";
 
-    let args = env::args().collect::<Vec<_>>();
-    if args[1] == "train" {
-        let start = std::time::Instant::now();
-        training::train::<MyAotudiffBackend>(
-            artifact_dir,
-            TrainingConfig::new(GeometryAutoEncoderConfig::new(1000), AdamConfig::new()),
-            device.clone(),
-        );
-        let duration = start.elapsed();
-        println!("Training time: {duration:?}");
-    } else if args[1] == "generate" {
-        let params: Vec<f32> = serde_json::from_str(&args[2]).unwrap();
-        crate::inference::infer::<MyBackend>(artifact_dir, device, params);
-    } else {
-        println!("Usage: {} train|infer [PARAMS]", args[0]);
+    match cli.command {
+        Commands::Train {} => {
+            let start = std::time::Instant::now();
+            training::train::<MyAotudiffBackend>(
+                artifact_dir,
+                TrainingConfig::new(GeometryAutoEncoderConfig::new(1000), AdamConfig::new()),
+                device.clone(),
+            );
+            let duration = start.elapsed();
+            println!("Training time: {duration:?}");
+        }
+        Commands::Generate { parameters } => {
+            crate::inference::infer::<MyBackend>(artifact_dir, device, parameters);
+        }
     }
 }
